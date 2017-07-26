f53f80a2e6e4f66a84886e7d38e5927bcc50dada
kayukawa-seita <k940805k.lab@gmail.com>
2017-07-26 21:44:23 +0900

    finish 03_mouseInteraction

--- 03_mouseInteraction/src/ofApp.cpp
+++ 03_mouseInteraction/src/ofApp.cpp
@@ -33,17 +33,11 @@ void ofApp::keyPressed(int key){
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
     if (key == OF_KEY_RETURN){
-        ofPoint pos, speed;
-        pos.x = ofGetWidth()/2;
-        pos.y = ofGetHeight()/2;
-        speed.x = ofRandom(-5, 5);
-        speed.y = ofRandom(-5, 5);
+        ofPoint pos = ofPoint(ofGetWidth()/2, ofGetHeight()/2);
+        ofPoint speed = ofPoint(ofRandom(-5, 5), ofRandom(-5, 5));
         float radius = ofRandom(30, 50);
-        ofVec3f color;
-        color[0] = ofRandom(0, 200);
-        color[1] = ofRandom(80, 200);
-        color[2] = ofRandom(160, 255);
-        
+        ofVec3f color = ofVec3f(ofRandom(0, 200), ofRandom(80, 200), ofRandom(160, 255));
+
         Particle newParticle(pos, radius, speed, color, gravity);
         particles.push_back(newParticle);
     }
@@ -70,31 +64,51 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-    for(int i=0; i<particles.size(); i++){
-        ofPoint pos = particles[i].getPos();
-        float radius = particles[i].getRadius();
-        ofPoint speed = particles[i].getSpeed();
-        float dist = ofDist(pos.x, pos.y, mouseX, mouseY);
-        float theta = atan2(speed.y,speed.x);
+    
+    if(particles.size() == 0) {
+        ofPoint newPos = ofPoint(mouseX, mouseY);
+        ofPoint newSpeed = ofPoint(ofRandom(-5, 5), ofRandom(-5, 5));
+        float newRadius = ofRandom(30, 50);
+        ofVec3f newColor = ofVec3f(ofRandom(0, 200), ofRandom(80, 200), ofRandom(160, 255));
         
-        if(dist<radius){
-            for(int j=0; j<3; j++){
-                ofPoint newSpeed;
-                newSpeed.x = sqrt(speed.x*speed.x + speed.y*speed.y) * cos(theta + PI*(j+1)/2);
-                newSpeed.y = sqrt(speed.x*speed.x + speed.y*speed.y) * sin(theta + PI*(j+1)/2);
-                float newRadius = radius;
-                ofVec3f newColor;
-                newColor[0] = ofRandom(0, 200);
-                newColor[1] = ofRandom(80, 200);
-                newColor[2] = ofRandom(160, 255);
-                
-                Particle newParticle(pos, newRadius, newSpeed, newColor, gravity);
-                particles.push_back(newParticle);
-            }
-            break;
-        }
+        Particle newParticle(newPos, newRadius, newSpeed, newColor, gravity);
+        particles.push_back(newParticle);
     }
 
+    else{
+        for(int i=0; i<particles.size(); i++){
+            ofPoint pos = particles[i].getPos();
+            float radius = particles[i].getRadius();
+            ofPoint speed = particles[i].getSpeed();
+            float dist = ofDist(pos.x, pos.y, mouseX, mouseY);
+            float theta = atan2(speed.y,speed.x);
+            
+            if(dist<radius){
+                for(int j=0; j<3; j++){
+                    ofPoint newSpeed;
+                    newSpeed.x = sqrt(speed.x*speed.x + speed.y*speed.y) * cos(theta + PI*(j+1)/2);
+                    newSpeed.y = sqrt(speed.x*speed.x + speed.y*speed.y) * sin(theta + PI*(j+1)/2);
+                    float newRadius = radius;
+                    ofVec3f newColor = ofVec3f(ofRandom(0, 200), ofRandom(80, 200), ofRandom(160, 255));
+                    
+                    Particle newParticle(pos, newRadius, newSpeed, newColor, gravity);
+                    particles.push_back(newParticle);
+                }
+                break;
+            }
+            
+            if(i == particles.size()-1){
+                ofPoint newPos = ofPoint(mouseX, mouseY);
+                ofPoint newSpeed = ofPoint(ofRandom(-5, 5), ofRandom(-5, 5));
+                float newRadius = ofRandom(30, 50);
+                ofVec3f newColor = ofVec3f(ofRandom(0, 200), ofRandom(80, 200), ofRandom(160, 255));
+                
+                Particle newParticle(newPos, newRadius, newSpeed, newColor, gravity);
+                particles.push_back(newParticle);
+                break;
+            }
+        }
+    }
 }
 
 //--------------------------------------------------------------
